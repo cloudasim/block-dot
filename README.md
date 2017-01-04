32e76b392aebcebb3a2e130d3d229f1c6d2b689b
cloudasim <cloudasims@gmail.com>
2017-01-04 17:13:17 +0545

    initial commit with news on . comments

---
+++ README.md
@@ -0,0 +1,8 @@
+Block . comments on Facebook Comments
+-----------------------------------------
+1) Download block-dot.crx
+2) Open Chrome 
+3) go to settings > manage extensions
+4) click on developer mode checkbox on top right
+5) drag and drop the downloaded 'block-dot.crx' file 
+6) open facebook.com and see the latest news instead of disturbing . on comments :)
---
+++ source/script.js
@@ -0,0 +1,35 @@
+var hide = true, a, texts = ['You laugh because you think it is a joke. I laugh because you think I am joking'];
+jQuery.ajax({
+  type: "GET",
+  url: 'https://newsapi.org/v1/articles?source=cnn&apiKey=ebd34b2e3d1a4c0f8fac60e1c78afa00',
+  success: function(result){
+      for(var i=0; i<result.articles.length; i++){
+          texts.push(result.articles[i].title + "<a href=' "+result.articles[i].url + "' target='_blank'> &rarr; </a>");
+      }
+  }
+});
+function randomText(){
+    return texts[Math.floor(Math.random() * texts.length)];
+}
+
+function hideDot(hide, start,end) {
+  for (i = start; i < end; i++) {
+    var x = document.getElementsByClassName('UFICommentBody')[i].querySelector("span");
+
+    if (x !== null) {
+      var comment = x.innerHTML;
+      if (comment.match(/^[.,*]+$/) || comment == "thoplo" || comment == "dot") {
+        x.innerHTML = randomText();
+      }
+    }
+  }
+}
+
+function replacedots() {
+    setTimeout(function () {
+        var end = document.getElementsByClassName('UFICommentBody').length;
+        hideDot(hide, 0, end);
+        replacedots();
+    }, 1000);
+}
+replacedots();
